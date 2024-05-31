0ace265c0cef5955e61315648cb3fdd76be5bddc
JustAnotherDevv <daniel_zarzecki@wp.pl>
2024-05-31 18:23:58 +0200

    added interfaces

---
+++ src/interfaces/IUltraBulkRenewal.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPLv3
+pragma solidity ~0.8.25;
+
+interface IUltraBulkRenewal {
+    function renewAll(
+        string[] calldata names,
+        uint256 duration,
+        uint256 price
+    ) external payable;
+}
