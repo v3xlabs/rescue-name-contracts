359e6d41651835ce5f384e05c7ac2751c85dae06
Lucemans <luc@lucemans.nl>
2024-06-01 16:46:22 +0000

    Optimize Contracts

--- src/RescueName.sol
+++ src/RescueName.sol
@@ -1,117 +1,81 @@
 // SPDX-License-Identifier: GPLv3
 pragma solidity ~0.8.17;
 
-// import "solmate/auth/Owned.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "solmate/auth/Owned.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
-// import "./ETHRegistrarController.sol";
-import "./interfaces/IUltraBulkRenewal.sol";
-import "./structs/RescueStructs.sol";
+import "./ETHRegistrarController.sol";
 
-contract RescueNameVault is Ownable, ReentrancyGuard, Initializable {
-    // PUBLIC VARIABLES
+contract RescueNameVault is Owned, ReentrancyGuard, Initializable {
     IUltraBulkRenewal controller;
-    uint256 public MIN_RENEWAL_TIME;
-    uint256 public constant MIN_DEADLINE = 1;
     uint256 public constant MAX_DEADLINE = 30;
-    // uint256 public GAS_LIMIT = xyz; // ToDo
+    uint256 public RENEW_DURATION = 365 days;
 
-    // MAPPINGS
-    mapping(string => bool) public listNames; // list id to name
+    mapping(string => bool) public nameList;
 
-    VaultConfig public vault;
-
-    constructor() Ownable(msg.sender) {
+    constructor() {
         _disableInitializers();
     }
 
-    function initialize(IUltraBulkRenewal _controller, uint256 _min_renewal, uint256 deadline, uint256 renewReward) external payable initializer {
+    function initialize(IETHRegistrarController _controller, uint256 _min_renewal, uint256 deadline, uint256 renewReward) external payable initializer {
         require(deadline >= MIN_DEADLINE && deadline <= MAX_DEADLINE , "Deadline overflow");
-        vault = VaultConfig(
-			msg.sender,
-            deadline, // in days, days to expiry has to be lower than val
-            msg.value,
-            renewReward,
-            0,
-            true
-            // ToDo
-		);
+
+        // TODO: Test if this gets the correct address
+        owner = msg.sender;
 
         controller = _controller;
         MIN_RENEWAL_TIME = _min_renewal;
     }
 
-    function getVault() public view returns (VaultConfig memory) {
-        // Add additional filtering etc
-        return vault;
-    }
-
-    function topUpVault() public payable {
-        require(msg.sender == vault.owner, "Caller is not vault owner");
-        require(msg.value != 0, "Can't add 0 to rewards");
-        vault.balance += msg.value;
-    }
-
-    function editVault(uint256 deadline) public payable {
-        require(msg.sender == vault.owner, "Caller is not vault owner");
+    function editDeadline(uint256 deadline) public payable onlyOwner {
+        require(deadline <= MAX_DEADLINE, "Deadline overflow");
         vault.deadline = deadline;
     }
 
-    function toggleVault() public payable {
-        require(msg.sender == vault.owner, "Caller is not vault owner");
-        !vault.isActive;
+    function toggleVault() public payable onlyOwner {
+        !isActive;
     }
 
-    function supplyList(string[] calldata names) public payable {
-        require(msg.sender == vault.owner, "Caller is not vault owner");
+    function supplyList(string[] calldata names) public payable onlyOwner {
         uint256 length = names.length;
         uint256 i = 0;
         while (i < length) {
-            listNames[names[i]] = true;
+            nameList[names[i]] = true;
         }
-        vault.listId = length;
     }
 
-    function toggleListName(string calldata name) public payable {
-        require(msg.sender == vault.owner, "Caller is not vault owner");
-        !listNames[name];
-    }    
+    function toggleName(string calldata name) public payable onlyOwner() {
+        !nameList[name];
+    }
 
-    // function executeMultiple(
-    //     uint256[] calldata vaultIds, 
-    //     string[][] calldata names,
-    //     uint256 price,
-    //     address payable multisig
-    // ) external payable nonReentrant() {
-    //     uint256 length = names.length;
-    //     uint256 i = 0;
-    //     while (i < length) {
-    //         executeSingle(vaultIds[i], names[i], price, multisig);
-    //     }
-    // }
-    
-    function executeSingle(
-        // uint256 vaultId,
+    function execute(
         string[] calldata names,
         uint256 price,
-        address payable multisig
+        address payable payee
     ) public payable nonReentrant() {
-        // VaultConfig storage vault = vaults[vaultId];
-        require(vault.balance >= vault.renewReward, "Not enough balance for reward payout");
-        // uint256 listId = vault.listId;
+        require(isActive, "Vault is not active");
+
         uint256 length = names.length;
         uint256 i = 0;
-        require(length < vault.listId, "Names index overflow");
+        uint256 total = price * length;
+
         while (i < length) {
-             require(listNames[names[i]], "Name not in provided vault");
+            require(nameList[names[i]], "Name not in provided vault");
+            // TODO: Check if we are currently (time) within deadline (expiryOfName - max_deadline)
+            controller.renew{value: price}(names[i], duration);
+            unchecked {
+                ++i;
+            }
         }
-        // We are assuming ultra bulk checks if reneval is possible, if price is correct etc and does reverts
-        controller.renewAll{value: price}(names, MIN_RENEWAL_TIME, price);
-        vault.balance - vault.renewReward;
-        multisig.transfer(msg.value);
+
+        // TODO: add check to prevent `price` from being too high
+
+        // TODO: figure out rewards
+        // multisig.transfer(msg.value);
     }
+
+    receive() external payable {}
 
     // @dev Not needed?
     // function refund() external payable onlyOwner {
--- src/RescueNameFactory.sol
+++ src/RescueNameFactory.sol
@@ -1,36 +1,24 @@
 // SPDX-License-Identifier: GPLv3
 pragma solidity ^0.8.19;
 
+import "solmate/auth/Owned.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 import "./RescueName.sol";
-import "./interfaces/IUltraBulkRenewal.sol";
-import "./structs/RescueStructs.sol";
 
-contract RescueNameFactory is Ownable, ReentrancyGuard {
+contract RescueNameFactory is Owned, ReentrancyGuard {
 
-    // PUBLIC VARIABLES
-    uint256 public MIN_RENEWAL_TIME;
-    uint256 public constant MIN_DEADLINE = 1;
-    uint256 public constant MAX_DEADLINE = 10;
-    uint256 public RENEW_DURATION = 365 days;
-    // uint256 public GAS_LIMIT = xyz; // ToDo
-    IUltraBulkRenewal controller;
-    uint256 vaultAmount;
-    uint256 listAmount = 1;
+    IETHRegistrarContorller public controller;
     address public rescueNameTemplate;
-
-    // MAPPINGS
-    mapping(uint256 => RescueNameVault) public vaults; // vault -> vault config
+    mapping(address => bool) public deployedContracts;
 
     event RescueNameVaultCreated(address newRescueNameVaultAddress);
 
-    constructor(address _rescueNameTemplate, IUltraBulkRenewal _controller, uint256 _min_renewal) Ownable(msg.sender) {
+    constructor(address _rescueNameTemplate, IETHRegistrarController _controller) {
         rescueNameTemplate = _rescueNameTemplate;
         controller = _controller;
-        MIN_RENEWAL_TIME = _min_renewal;
+        owner = msg.sender;
     }
 
     function setRescueNameAddress(address _rescueNameTemplate) public onlyOwner {
@@ -41,54 +29,20 @@ contract RescueNameFactory is Ownable, ReentrancyGuard {
         address clone = Clones.clone(rescueNameTemplate);
         RescueNameVault(clone).initialize(controller, _min_renewal, deadline, renewReward);
         emit RescueNameVaultCreated(clone);
-    }
 
-    function vaultById(uint256 vaultId) public view returns(VaultConfig memory) {
-        RescueNameVault rescueContract = vaults[vaultId];
-        VaultConfig memory config = rescueContract.getVault();
-        return config;
-    }
-
-    function topUpVault(uint256 vaultId) public payable {
-        RescueNameVault rescueContract = vaults[vaultId];
-        rescueContract.topUpVault();
-    }
-
-    function editVault(uint256 vaultId, uint256 deadline) public payable {
-        RescueNameVault rescueContract = vaults[vaultId];
-        rescueContract.editVault(deadline);
-    }
-
-    function supplyList(uint256 vaultId, string[] calldata names) public payable {
-        RescueNameVault rescueContract = vaults[vaultId];
-        rescueContract.supplyList(names);
-    }
-
-    function toggleList(uint256 vaultId) public payable {
-        RescueNameVault rescueContract = vaults[vaultId];
-        rescueContract.toggleVault();
+        deployedContracts[clone] = true;
     }
 
     function execute(
-        uint256[] calldata vaultIds, 
+        RescueNameVault[] calldata vaults, 
         string[][] calldata names,
         uint256 price,
-        address payable multisig
+        address payable payee
     ) public payable nonReentrant() {
         uint256 length = names.length;
         uint256 i = 0;
         while (i < length) {
-            executeSingle(vaultIds[i], names[i], price, multisig);
+            vaults[i].execute(names[i], price, payee);
         }
-    }
-
-    function executeSingle(
-        uint256 vaultId,
-        string[] calldata names,
-        uint256 price,
-        address payable multisig
-    ) public payable nonReentrant() {
-        RescueNameVault rescueContract = vaults[vaultId];
-        rescueContract.executeSingle(names, price, multisig);
     }
 }
