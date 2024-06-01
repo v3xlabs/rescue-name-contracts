8b1ed5192beb117df14d4e6a2759b2e055f80b94
Lucemans <luc@lucemans.nl>
2024-06-01 20:45:48 +0000

    Refactor into single contract

--- src/RescueNameFactory.sol
+++ src/RescueNameFactory.sol
@@ -5,37 +5,39 @@ import "solmate/auth/Owned.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-import "./RescueName.sol";
 import "./interfaces/IETHRegistrarController.sol";
+import "./interfaces/IBaseRegistrar.sol";
 
 contract RescueNameFactory is Owned, ReentrancyGuard {
 
-    IETHRegistrarController public controller;
-    address payable public rescueNameTemplate;
-    mapping(RescueNameVault => bool) public deployedContracts;
+    event NameAdded(uint256 vault, string name);
+    event NameRemoved(uint256 vault, string name);
 
-    event RescueNameVaultCreated(RescueNameVault newRescueNameVaultAddress);
+    IBaseRegistrar public constant baseregistrar = IBaseRegistrar(0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85);
+    IETHRegistrarController public constant controller = IETHRegistrarController(0x253553366Da8546fC250F225fe3d25d0C782303b);
 
-    constructor(address payable _rescueNameTemplate, IETHRegistrarController _controller) Owned(msg.sender) {
-        rescueNameTemplate = _rescueNameTemplate;
-        controller = _controller;
-        owner = msg.sender;
-    }
+    uint256 public constant MAX_DEADLINE = 30;
+    uint256 public constant RENEW_DURATION = 365 days;
 
-    function setRescueNameAddress(address payable _rescueNameTemplate) public onlyOwner {
-        rescueNameTemplate = _rescueNameTemplate;
-    }
+    uint256 public lastVaultId = 0;
+    mapping(uint256 => address) public vaultToOwner;
+    mapping(uint256 => bool) public vaultIsActive;
+    mapping(uint256 => mapping(string => bool)) public vaultNameList;
+    mapping(uint256 => uint256) public vaultDeadline;
+    mapping(uint256 => uint256) public vaultRenewReward;
+    mapping(uint256 => uint256) public vaultBalance;
+
+    event RescueNameVaultCreated(uint256 vaultId, address owner);
+
+    constructor() Owned(msg.sender) {}
 
     function createVault(uint256 deadline, uint256 renewReward) public payable {
-        RescueNameVault clone = RescueNameVault(payable(Clones.clone(rescueNameTemplate)));
-        clone.initialize(controller, deadline, renewReward);
-        emit RescueNameVaultCreated(clone);
-
-        deployedContracts[clone] = true;
+        emit RescueNameVaultCreated(lastVaultId, msg.sender);
+        lastVaultId++;
     }
 
     function execute(
-        RescueNameVault[] calldata vaults, 
+        uint256[] calldata vaults, 
         string[][] calldata names,
         uint256 price,
         address payable payee
@@ -43,7 +45,63 @@ contract RescueNameFactory is Owned, ReentrancyGuard {
         uint256 length = names.length;
         uint256 i = 0;
         while (i < length) {
-            vaults[i].execute(names[i], price, payee);
+            require(vaultIsActive[vaults[i]], "Vault is not active");
+
+            uint256 length = names.length;
+            uint256 j = 0;
+            uint256 total = price * length;
+
+            while (j < length) {
+                require(vaultNameList[vaults[i]][names[i][j]], "Name not in vault");
+                // TODO: Check if we are currently (time) within deadline (expiryOfName - max_deadline)
+                bytes32 labelhash = keccak256(abi.encodePacked(names[i][j]));
+                baseregistrar.nameExpires(uint256(labelhash));
+
+                controller.renew{value: price}(names[i][j], RENEW_DURATION * 24 * 60 * 60); 
+                unchecked {
+                    ++i;
+                }
+            }
+
+            // TODO: add check to prevent `price` from being too high
+
+            // uint256 reward = // TODO: calculate reward
+            // payable(payee).transfer(reward);
+            // vaultBalance[vaults[i]] -= reward;
         }
     }
+
+    /* Vault Specific Functions */
+
+    function topupVault(uint256 vault) public payable {
+        vaultBalance[vault] += msg.value;
+    }
+
+    function toggleVaultActive(uint256 vault) public payable onlyOwner {
+        vaultIsActive[vault] = !vaultIsActive[vault];
+    }
+
+    function toggleName(uint256 vault, string calldata name) public payable onlyOwner() {
+        vaultNameList[vault][name] = !vaultNameList[vault][name];
+
+        if (vaultNameList[vault][name]) {
+            emit NameAdded(vault, name);
+        } else {
+            emit NameRemoved(vault, name);
+        }
+    }
+
+    function supplyList(uint256 vault, string[] calldata names) public payable onlyOwner {
+        uint256 length = names.length;
+        uint256 i = 0;
+        while (i < length) {
+            vaultNameList[vault][names[i]] = true;
+            emit NameAdded(vault, names[i]);
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+
 }
